cb25a6f670a6e7a8bfa901307a230699963eb27d
shade2903 <nighshade2903@gmail.com>
2021-09-01 16:48:03 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const arr = [1,10,4,2];
+const meanValueArr  = (arr) =>{ //метод вычисляющий среднее значение элементов массива
+    let summ = 0;
+    for(let i = 0; i < arr.length; i++){
+        summ += arr[i];
+    }
+    console.log(summ/arr.length);
+}
+meanValueArr(arr);
+Array.prototype.meanValue = function () { 
+    let summ = 0;
+    for(let i = 0; i < this.length; i++){
+        summ += arr[i];
+    }
+    console.log(summ/this.length);
+}
+arr.meanValue();
+
+//максимальное число
+// const a = parseInt(prompt("enter first number"));
+// const b = parseInt(prompt("enter second number"));
+// const c = parseInt(prompt("enter third number"));
+
+function max(a,b,c){
+    if (a == undefined){
+        return undefined;
+    }
+    if(b == undefined){
+        return a;
+    }
+    if(c== undefined){
+        return a < b ? a : b;
+    }
+    if (a>b && a > c){
+        return a;
+    }
+   
+    return b > c ? b : c;
+}
+// alert("Max number: "+ max(a,b,c));
+
+//Игра угадай число
+let randomNumber = Math.round((Math.random()*100));
+console.log(randomNumber);
